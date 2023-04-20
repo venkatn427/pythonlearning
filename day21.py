60779578c21a616d88a097fcc4789081286bbe7d
Venkat Nelluri <nvenkat427@gmail.com>
2023-04-20 08:47:41 -0500

    modified:   day21.py

--- day21.py
+++ day21.py
@@ -16,3 +16,41 @@ Once the user is done, the code should return the largest even number in the
 inputted numbers. If there is no even number in the list, the code should return 
 the average of all the five numbers."""
 
+def even_or_average(nums):
+    """
+    Asks user to input five numbers and then returns the largest even number,
+    or if none are even, the average of all the numbers.
+    """
+    even_nums = [num for num in nums if num % 2 == 0]
+    if even_nums:
+        return max(even_nums)
+    else:
+        return sum(nums)/len(nums)
+
+print(even_or_average([2, 5, 7, 9, 11]))
+
+
+# Test the make_tuples function with two equal lists of integers.
+def test_make_tuples_equal_lists():
+    assert make_tuples([1, 2, 3], [4, 5, 6]) == [(1, 4), (2, 5), (3, 6)]
+
+# Test the make_tuples function with two equal lists of strings.
+def test_make_tuples_equal_lists_strings():
+    assert make_tuples(['Hello', 'world'], ['Python', 'programming']) == [('Hello', 'Python'), ('world', 'programming')]
+
+# Test the make_tuples function with two lists of different lengths.
+def test_make_tuples_different_length_lists():
+    assert make_tuples([1, 2, 3], [4, 5]) == "Invalid Input"
+
+# Test the even_or_average function by providing five numbers, one of which is even.
+def test_even_or_average_single_even_number(monkeypatch):
+    input_values = [1, 3, 5, 8, 9]
+    monkeypatch.setattr('builtins.input', lambda _: str(input_values.pop(0)))
+    assert even_or_average() == 8
+
+# Test the even_or_average function by providing five numbers, none of which are even.
+def test_even_or_average_no_even_numbers(monkeypatch):
+    input_values = [1, 3, 5, 7, 9]
+    monkeypatch.setattr('builtins.input', lambda _: str(input_values.pop(0)))
+    assert even_or_average() == 5.0
+
